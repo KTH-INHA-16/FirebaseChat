510df031bb6038e7a9dca6d736732c660a0016b6
MatthewAsleep <easydream@asleep.ai>
2022-08-17 15:52:05 +0900

    🐛 firebase configure error

--- FirebaseChat/FirebaseChatApp.swift
+++ FirebaseChat/FirebaseChatApp.swift
@@ -8,17 +8,13 @@
 import SwiftUI
 import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
-
 @main
 struct FirebaseChatApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
